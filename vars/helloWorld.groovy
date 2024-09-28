3aa5ec1131333a7eddbbe1227762a66ecb7874fa
Rahul <r1111111r@gmail.com>
2024-09-28 15:09:27 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -8,8 +8,7 @@ def call(String name) {
    // powershell ' dir env: '
     powershell """
     
-    Write-Host "$name"
-    Write-Host "\$name"
+    Write-Host  "Hello" + "$name"
     
     """
     //sh ' echo Hello ${name} '
