8c68f0191d07b7e91e0d8f3ec5b03c6b46d60aff
Rahul <r1111111r@gmail.com>
2024-09-28 13:01:21 +0530

    param string

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,9 @@
 //def call(Map config = [:]) {
 //    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 //}
-def call() {
-    powershell ' Write-Host "Hello" '
+//def call() {
+//    powershell ' Write-Host "Hello" '
+//}
+def call(String name) {
+    powershell ' Write-Host "Hello ${name}" '
 }
